ba54aaf1f94a7c651d8dbbcbed29c04ab781562c
Wenzel <wenzel_contato@hotmail.com>
2022-02-05 10:48:14 -0300

    calculdora feita com React Hooks

---
+++ src/components/button.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./button.css";
+
+const Button = (props) => {
+  return (
+    <button
+      onClick={(e) => props.click && props.click(props.label)}
+      className={`
+            button 
+            ${props.operation ? "operation" : ""}
+            ${props.double ? "double" : ""}
+            ${props.triple ? "triple" : ""}
+
+        `}
+    >
+      {props.label}
+    </button>
+  );
+};
+
+export default Button;
